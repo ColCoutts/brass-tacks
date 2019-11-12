2cd6ae7a618d94e187ae683b8291f4a2db07d66e
Colin Coutts <colintcoutts@gmail.com>
2019-11-12 07:23:55 -0800

    firebase initialized google sign up in sign in cmpnt

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
   constructor(props) {
@@ -48,6 +49,11 @@ class SignIn extends Component {
           />
           <CustomButton type='submit' value='Submit Form'>
             Sign In
+          </CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            {''}
+            Sign in with Google
+            {''}
           </CustomButton>
         </form>
       </div>
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyCUyhU01XL89RrK1CGza-vz1BrsHEawGHE',
+  authDomain: 'brass-tacks-db.firebaseapp.com',
+  databaseURL: 'https://brass-tacks-db.firebaseio.com',
+  projectId: 'brass-tacks-db',
+  storageBucket: 'brass-tacks-db.appspot.com',
+  messagingSenderId: '844086741335',
+  appId: '1:844086741335:web:7aa53922e5f1ec52bc9612',
+  measurementId: 'G-1Y79BD1474'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
