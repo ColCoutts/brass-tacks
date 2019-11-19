1bd61fef44d168df030c6c0ead45579089652902
Colin Coutts <colintcoutts@gmail.com>
2019-11-19 07:20:54 -0800

    started setting up redux, store in place user reducer and mapstate to props for currentuser

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       currentUser: null
     };
@@ -46,7 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
